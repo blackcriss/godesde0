a9b32ccdb8dd6cac5a59aadf7bb1f9d3290bb798
Black Criss <cristianypv@hotmail.com>
2023-08-02 13:56:05 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/blackcriss/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto(1020)
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
